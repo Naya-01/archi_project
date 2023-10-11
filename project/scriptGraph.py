44aad309a1bb4ba6dad04d5a184e0631e26b2157
Naya-01 <abarkan.rayan@gmail.com>
2023-10-11 21:39:26 +0200

    refactoring python script  && read all files in the results folder

--- project/scriptGraph.py
+++ project/scriptGraph.py
@@ -1,25 +1,32 @@
 import os
 import re
 
-# Absolute file path
-absolute_path = os.path.abspath('./results/test.txt')
 
-with open(absolute_path, 'r') as fichier:
-    content = fichier.read()
+def parse(file):
+    lines = file.split('\n')
+    start_index = lines.index(
+        "       Value   Percentile   TotalCount 1/(1-Percentile)")
+
+    pattern = r".*#\[Mean\s+=.*"
+
+    for line in lines[start_index+1:]:
+
+        if re.match(pattern, line):
+            break
+
+        if line.strip() != "":
+            colonnes = line.split()
+            value, percentile, total_count, inverse_percentile = colonnes[:4]
+            # only to see the parse results
+            print(
+                f"Value: {value}, Percentile: {percentile}, TotalCount: {total_count}, 1/(1-Percentile): {inverse_percentile}")
 
 
-lines = content.split('\n')
-start_index = lines.index("       Value   Percentile   TotalCount 1/(1-Percentile)")
+folder = "results"
+for file in os.listdir(folder):
+    if os.path.isfile(os.path.join(folder, file)):
+        with open(os.path.join(folder, file), 'r') as open_file:
+            content = open_file.read()
 
-pattern = r".*#\[Mean\s+=.*"
-
-for line in lines[start_index+1:]:
-
-    if re.match(pattern, line): 
-        break
-
-    if line.strip() != "":
-        colonnes = line.split()
-        value, percentile, total_count, inverse_percentile = colonnes[:4]
-        print(f"Value: {value}, Percentile: {percentile}, TotalCount: {total_count}, 1/(1-Percentile): {inverse_percentile}") #only to see the parse results
-
+            print(f"File name : {file}")
+            parse(content)
