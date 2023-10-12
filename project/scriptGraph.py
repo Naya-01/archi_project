c010ce5ed31b1d418f8c70878a66d5945753575c
Naya-01 <abarkan.rayan@gmail.com>
2023-10-12 15:07:26 +0200

    add parsing && refactoring

--- project/scriptGraph.py
+++ project/scriptGraph.py
@@ -2,25 +2,44 @@ import os
 import re
 
 
-def parse(file):
-    lines = file.split('\n')
+def parse_In_Details(content):
+    lines = content.split('\n')
     start_index = lines.index(
-        "       Value   Percentile   TotalCount 1/(1-Percentile)")
+        "Value   Percentile   TotalCount 1/(1-Percentile)")
 
     pattern = r".*#\[Mean\s+=.*"
 
     for line in lines[start_index+1:]:
-
         if re.match(pattern, line):
             break
 
-        if line.strip() != "":
+        colonnes = line.split()
+        value, percentile, total_count, inverse_percentile = colonnes[:4]
+        # only to see the parse results
+        print(f"Value: {value}, Percentile: {percentile}, TotalCount: {total_count}, 1/(1-Percentile): {inverse_percentile}")
+            
+
+def parse_Latency_Dist(content):
+    lines = content.split('\n')
+    start_index = lines.index(
+        "Latency Distribution (HdrHistogram - Recorded Latency)")
+
+    end_index = lines.index("Detailed Percentile spectrum:")
+
+    for line in lines[start_index+1:end_index]:
             colonnes = line.split()
-            value, percentile, total_count, inverse_percentile = colonnes[:4]
+            percent, latency = colonnes[:2]
             # only to see the parse results
             print(
-                f"Value: {value}, Percentile: {percentile}, TotalCount: {total_count}, 1/(1-Percentile): {inverse_percentile}")
+                f"percent: {percent}, latency: {latency}")
 
+
+def clear_content(content):
+    lines = content.split('\n')
+    non_empty_lines = [line.strip() for line in lines if line.strip() != '']
+    text_without_empty_lines = '\n'.join(non_empty_lines)
+    
+    return text_without_empty_lines
 
 folder = "results"
 for file in os.listdir(folder):
@@ -29,4 +48,8 @@ for file in os.listdir(folder):
             content = open_file.read()
 
             print(f"File name : {file}")
-            parse(content)
+            content = clear_content(content)
+            parse_Latency_Dist(content)
+            parse_In_Details(content)
+
+
