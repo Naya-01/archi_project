eedf3d3263383e9f2e3c7b245a6224c6965a2348
Naya-01 <abarkan.rayan@gmail.com>
2023-10-17 10:03:01 +0200

    update scripts

--- project/make_plots.sh
+++ project/make_plots.sh
@@ -3,42 +3,43 @@
 echo "Executing performance evaluation for generated Lua scripts..."
 
 # Créer le répertoire pour les résultats
-results_dir="results"
-mkdir -p "$results_dir"
+# results_dir="results"
+# mkdir -p "$results_dir"
 
-# Paramètres à tester
-numRounds=("5")
-file_sizes=("8" "16" "32" "64")
-key_sizes=("8" "16" "32" "64")
+# # Paramètres à tester
+# numRounds=("5")
+# file_sizes=("8" "16" "32" "64")
+# key_sizes=("8" "16" "32" "64")
 
-# Durée du test (en secondes)
-test_duration=30
+# # Durée du test (en secondes)
+# test_duration=30
 
-# Itérer sur les combinaisons de paramètres
-for numRound in "${numRounds[@]}"; do
-  for size in "${file_sizes[@]}"; do
-    for key in "${key_sizes[@]}"; do
+# # Itérer sur les combinaisons de paramètres
+# for numRound in "${numRounds[@]}"; do
+#   for size in "${file_sizes[@]}"; do
+#     for key in "${key_sizes[@]}"; do
 
-      # Chemin du script Lua généré
-      script_file="script_${key}_${size}_${numRound}.lua"
+#       # Chemin du script Lua généré
+#       script_file="script_${key}_${size}_${numRound}.lua"
 
-      # Générer le nom du fichier de sortie
-      output_file="$results_dir/numRound${numRound}_size${size}_key${key}.csv"
+#       # Générer le nom du fichier de sortie
+#       output_file="$results_dir/numRound${numRound}_size${size}_key${key}.csv"
 
-      for ((i=1; i<=3; i++)); do
-        echo "tes test $i"
-        ../wrk2-DeathStarBench/wrk http://localhost:8888/ -d$test_duration --latency -R100 -s ./wrk_scripts/$script_file >> $output_file
-      done
+#       for ((i=1; i<=3; i++)); do
+#         echo "tes test $i"
+#         ../wrk2-DeathStarBench/wrk http://localhost:8888/ -d$test_duration --latency -R100 -s ./wrk_scripts/$script_file >> $output_file
+#       done
 
-      echo "Test completed for  file_size=${size}, key=${key}"
+#       echo "Test completed for  file_size=${size}, key=${key}"
 
-    done
-  done
-done
+#     done
+#   done
+# done
 
 echo "Performance evaluation completed."
 
 # Appeler le script Python pour générer les graphiques
 # python scriptGraph.py
 #python keyScript.py
-npf-run --test newTest.npf --single-output "data/measurements.csv"
+python "python_scripts/scriptCsv.py"
+#npf-run --test newTest.npf --single-output "data/measurements.csv"
---
+++ project/python_scripts/scriptCsv.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+datas = pd.read_csv("../data/measurements.csv")
+
+mean_latency = datas.groupby('key_size')['LATENCY'].mean() * 1000
+error = datas.groupby('key_size')['LATENCY'].std() * 1000
+plt.figure(figsize=(10, 6))
+
+mean_latency.plot(kind='line', color='blue')
+plt.scatter(mean_latency.index, mean_latency.values, color='blue', label='Points', s=50)
+plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', color='blue', elinewidth=2, capsize=5, label='Barres d\'erreur')
+plt.xlabel('Key size')
+plt.ylabel('LATENCY (ms)')
+plt.title('LATENCY in line with Key size')  
+plt.grid(True)
+plt.savefig("../plots/keyplot.PNG")
+plt.close()
+
+
+
+
+mean_latency = datas.groupby('file_size')['LATENCY'].mean() * 1000
+error = datas.groupby('file_size')['LATENCY'].std() * 1000
+plt.figure(figsize=(10, 6))
+
+mean_latency.plot(kind='line', color='blue')
+plt.scatter(mean_latency.index, mean_latency.values, color='blue', label='Points', s=50)
+plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', color='blue', elinewidth=2, capsize=5, label='Barres d\'erreur')
+plt.xlabel('File size')
+plt.ylabel('LATENCY (ms)')
+plt.title('LATENCY in line with File size')  
+plt.grid(True)
+plt.savefig("../plots/fileplot.PNG")
+plt.close()
+
+
+
+
+mean_latency = datas.groupby('threads')['LATENCY'].mean() * 1000
+error = datas.groupby('threads')['LATENCY'].std() * 1000
+plt.figure(figsize=(10, 6))
+
+mean_latency.plot(kind='line', color='blue')
+plt.scatter(mean_latency.index, mean_latency.values, color='blue', label='Points', s=50)
+plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', color='blue', elinewidth=2, capsize=5, label='Barres d\'erreur')
+plt.xlabel('threads')
+plt.ylabel('LATENCY (ms)')
+plt.title('LATENCY in line with File size')  
+plt.grid(True)
+plt.savefig("../plots/threadsPlot.PNG")
+plt.close()
+
+
+
+
+
+
+
+
+
+result = datas.groupby(['file_size', 'threads'])['LATENCY'].mean().unstack() * 1000
+result.plot(kind='area', stacked=True, figsize=(10, 6))
+plt.xlabel('file size')
+plt.ylabel('LATENCY (ms)')
+plt.title('LATENCY by File Size and Threads')
+plt.legend(title='Threads')
+plt.grid(True)
+plt.savefig("../plots/fileThreadsplots.PNG")
+plt.close()
+
+
+
+result = datas.groupby(['key_size', 'threads'])['LATENCY'].mean().unstack() * 1000
+result.plot(kind='area', stacked=True, figsize=(10, 6))
+plt.xlabel('key_size')
+plt.ylabel('LATENCY (ms)')
+plt.title('LATENCY by Key Size and Threads')
+plt.legend(title='Threads')
+plt.grid(True)
+plt.savefig("../plots/keyThreadsplots.PNG")
+plt.close()
+
+
+
+
+
+
+
+
+
