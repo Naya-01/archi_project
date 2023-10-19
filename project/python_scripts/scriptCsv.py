47725ea2cab71d61b2f039e80877fe0c7885ec62
JordanHanotiaux <jordan.hanotiaux@student.uclouvain.be>
2023-10-19 13:27:55 +0200

    Request/sec add plots + Different debit

--- project/python_scripts/scriptCsv.py
+++ project/python_scripts/scriptCsv.py
@@ -3,102 +3,167 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-
 datas = pd.read_csv("./data/measurements.csv")
 datas['LATENCY'] = datas['LATENCY'] * 1000
+datas['REQ'] = datas['REQ'] * 1000
+# Regrouper les données par 'debits'
+grouped = datas.groupby('debits')
 
-mean_latency = datas.groupby('key_size')['LATENCY'].mean() 
-error = datas.groupby('key_size')['LATENCY'].std() 
+# Latency/keysize
 plt.figure(figsize=(10, 6))
 
-mean_latency.plot(kind='line', color='blue')
-plt.scatter(mean_latency.index, mean_latency.values, color='blue', label='Points', s=50)
-plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', color='blue', elinewidth=2, capsize=5, label='Barres d\'erreur')
+for debits, group_data in grouped:
+    mean_latency = group_data.groupby('key_size')['LATENCY'].mean()
+    error = group_data.groupby('key_size')['LATENCY'].std()
+
+    mean_latency.plot(kind='line', label=f'Debits: {debits}')
+    plt.scatter(mean_latency.index, mean_latency.values)
+    plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', elinewidth=2, capsize=5)
+
 plt.xlabel('Key size (bytes)')
 plt.ylabel('LATENCY (ms)')
-plt.title('LATENCY in line with Key size')  
+plt.title('LATENCY in line with Key size for Different Debits')
 plt.grid(True)
-plt.savefig("./plots/keyplot.PNG")
+plt.legend()
+plt.savefig("./plots/keyplot_all_debits_latency.PNG")
 plt.close()
 
-
-
-
-mean_latency = datas.groupby('file_size')['LATENCY'].mean() 
-error = datas.groupby('file_size')['LATENCY'].std() 
+# Request/keysize
 plt.figure(figsize=(10, 6))
 
-mean_latency.plot(kind='line', color='blue')
-plt.scatter(mean_latency.index, mean_latency.values, color='blue', label='Points', s=50)
-plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', color='blue', elinewidth=2, capsize=5, label='Barres d\'erreur')
+for debits, group_data in grouped:
+    mean_latency = group_data.groupby('key_size')['REQ'].mean()
+    error = group_data.groupby('key_size')['REQ'].std()
+
+    mean_latency.plot(kind='line', label=f'Debits: {debits}')
+    plt.scatter(mean_latency.index, mean_latency.values)
+    plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', elinewidth=2, capsize=5)
+
+plt.xlabel('Key size (bytes)')
+plt.ylabel('REQUEST (ms)')
+plt.title('REQUEST in line with Key size for Different Debits')
+plt.grid(True)
+plt.legend()
+plt.savefig("./plots/keyplot_all_debits_requests.PNG")
+plt.close()
+
+# Latency/FileSize
+plt.figure(figsize=(10, 6))
+
+for debits, group_data in grouped:
+    mean_latency = group_data.groupby('file_size')['LATENCY'].mean()
+    error = group_data.groupby('file_size')['LATENCY'].std()
+
+    mean_latency.plot(kind='line', label=f'Debits: {debits}')
+    plt.scatter(mean_latency.index, mean_latency.values)
+    plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', elinewidth=2, capsize=5)
+
 plt.xlabel('File size (bytes)')
 plt.ylabel('LATENCY (ms)')
-plt.title('LATENCY in line with File size')  
+plt.title('LATENCY in line with Key size for Different Debits')
 plt.grid(True)
-plt.savefig("./plots/fileplot.PNG")
+plt.legend()
+plt.savefig("./plots/fileplot_all_debits_latency.PNG")
 plt.close()
 
-
-
-
-mean_latency = datas.groupby('threads')['LATENCY'].mean() 
-error = datas.groupby('threads')['LATENCY'].std() 
+# Request/FileSize
 plt.figure(figsize=(10, 6))
 
-mean_latency.plot(kind='line', color='blue')
-plt.scatter(mean_latency.index, mean_latency.values, color='blue', label='Points', s=50)
-plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', color='blue', elinewidth=2, capsize=5, label='Barres d\'erreur')
-plt.xlabel('threads')
-plt.ylabel('LATENCY (ms)')
-plt.title('LATENCY in line with Threads')  
+for debits, group_data in grouped:
+    mean_latency = group_data.groupby('file_size')['REQ'].mean()
+    error = group_data.groupby('file_size')['REQ'].std()
+
+    mean_latency.plot(kind='line', label=f'Debits: {debits}')
+    plt.scatter(mean_latency.index, mean_latency.values)
+    plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', elinewidth=2, capsize=5)
+
+plt.xlabel('File size (bytes)')
+plt.ylabel('Req (ms)')
+plt.title('Req in line with Key size for Different Debits')
 plt.grid(True)
-plt.savefig("./plots/threadsPlot.PNG")
-plt.close()
-
-
-
-plt.figure(figsize=(18, 6))
-
-plt.subplot(131)  # Premier sous-graphique
-plt.imshow(plt.imread("./plots/keyplot.PNG"))
-plt.axis('off')
-plt.title('Compare by key Size')
-
-plt.subplot(132)  # Deuxième sous-graphique
-plt.imshow(plt.imread("./plots/fileplot.PNG"))
-plt.axis('off')
-plt.title('Compare by File Size')
-
-plt.subplot(133) 
-plt.imshow(plt.imread("./plots/threadsPlot.PNG"))
-plt.axis('off')
-plt.title('Compare by Threads')
-
-plt.tight_layout()
-
-plt.suptitle('LATENCY comparisons', fontsize=16)
-plt.savefig("./plots/compare3.PNG")
-plt.close()
-
-
-
-
-
-grouped = datas.groupby(['file_size', 'key_size']).LATENCY.mean().reset_index() 
-
-for key in grouped['key_size'].unique():
-    subset = grouped[grouped['key_size'] == key]
-    plt.plot(subset['file_size'], subset['LATENCY'], label=f"Key Size: {key}", marker='o')
-
 plt.legend()
-plt.xlabel("File Size (bytes)")
-plt.ylabel("Latency (ms)")
-plt.title("Average Latency vs. File Size for Different Key Sizes")
-plt.grid(True)
-plt.savefig("./plots/latency_vs_filesize.PNG")
+plt.savefig("./plots/fileplot_all_debits_request.PNG")
 plt.close()
 
+# Latency/Threads
+plt.figure(figsize=(10, 6))
 
+for debits, group_data in grouped:
+    mean_latency = group_data.groupby('threads')['LATENCY'].mean()
+    error = group_data.groupby('threads')['LATENCY'].std()
+
+    mean_latency.plot(kind='line', label=f'Debits: {debits}')
+    plt.scatter(mean_latency.index, mean_latency.values)
+    plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', elinewidth=2, capsize=5)
+
+plt.xlabel('File size (bytes)')
+plt.ylabel('LATENCY (ms)')
+plt.title('LATENCY in line with Key size for Different Debits')
+plt.grid(True)
+plt.legend()
+plt.savefig("./plots/threadsplot_all_debits_latency.PNG")
+plt.close()
+
+# Request/Threads
+plt.figure(figsize=(10, 6))
+
+for debits, group_data in grouped:
+    mean_latency = group_data.groupby('threads')['REQ'].mean()
+    error = group_data.groupby('threads')['REQ'].std()
+
+    mean_latency.plot(kind='line', label=f'Debits: {debits}')
+    plt.scatter(mean_latency.index, mean_latency.values)
+    plt.errorbar(mean_latency.index, mean_latency.values, yerr=error.values, fmt='o', elinewidth=2, capsize=5)
+
+plt.xlabel('File size (bytes)')
+plt.ylabel('REQ (ms)')
+plt.title('REQ in line with Key size for Different Debits')
+plt.grid(True)
+plt.legend()
+plt.savefig("./plots/threadsplot_all_debits_request.PNG")
+plt.close()
+
+#file vs key / Latency
+
+grouped_debits = datas.groupby('debits')
+
+for debits, group_data in grouped_debits:
+    plt.figure(figsize=(10, 6))
+
+    # Regrouper les données par 'file_size' et 'key_size' et calculer la moyenne de la latence
+    grouped = group_data.groupby(['file_size', 'key_size'])['LATENCY'].mean().reset_index()
+
+    for key_size in grouped['key_size'].unique():
+        subset = grouped[grouped['key_size'] == key_size]
+        plt.plot(subset['file_size'], subset['LATENCY'], label=f"Key Size: {key_size}", marker='o')
+
+    plt.legend()
+    plt.xlabel("File Size (bytes)")
+    plt.ylabel("Average Latency (ms)")
+    plt.title(f"Average Latency vs. File Size for Different Key Sizes (Debits: {debits})")
+    plt.grid(True)
+    plt.savefig(f"./plots/latency_vs_filesize_debits_{debits}.PNG")
+    plt.close()
+
+#file vs key / REQ
+
+for debits, group_data in grouped_debits:
+    plt.figure(figsize=(10, 6))
+
+    # Regrouper les données par 'file_size' et 'key_size' et calculer la moyenne de la latence
+    grouped = group_data.groupby(['file_size', 'key_size'])['REQ'].mean().reset_index()
+
+    for key_size in grouped['key_size'].unique():
+        subset = grouped[grouped['key_size'] == key_size]
+        plt.plot(subset['file_size'], subset['REQ'], label=f"Key Size: {key_size}", marker='o')
+
+    plt.legend()
+    plt.xlabel("File Size (bytes)")
+    plt.ylabel("Average REQ (ms)")
+    plt.title(f"Average REQ vs. File Size for Different Key Sizes (Debits: {debits})")
+    plt.grid(True)
+    plt.savefig(f"./plots/req_latency_vs_filesize_debits_{debits}.PNG")
+    plt.close()
 
 
 grouped = datas.groupby(['key_size', 'file_size']).LATENCY.mean().reset_index()
@@ -116,35 +181,70 @@ plt.grid(True)
 plt.savefig("./plots/latency_vs_keysize.PNG")
 plt.close()
 
+for debits, group_data in grouped_debits:
+    plt.figure(figsize=(10, 6))
+    # Grouper par 'key_size' et 'threads' puis calculer la latence moyenne
+    grouped = group_data.groupby(['key_size', 'threads'])['LATENCY'].mean().reset_index()
 
+    for thread in grouped['threads'].unique():
+        subset = grouped[grouped['threads'] == thread]
+        plt.plot(subset['key_size'], subset['LATENCY'], label=f"Threads: {thread}", marker='o')
 
+    plt.legend()
+    plt.xlabel("Key Size (bytes)")
+    plt.ylabel("Average Latency (ms)")
+    plt.title(f"Average Latency vs. Key Size for Different Number of Threads (Debits: {debits})")
+    plt.grid(True)
+    plt.savefig(f"./plots/latency_vs_keysize_threads_debits_{debits}.PNG")
+    plt.close()
 
+for debits, group_data in grouped_debits:
+    plt.figure(figsize=(10, 6))
+    # Grouper par 'key_size' et 'threads' puis calculer la latence moyenne
+    grouped = group_data.groupby(['key_size', 'threads'])['REQ'].mean().reset_index()
 
-# Grouper par key_size et threads puis calcule la latence moyenne
-grouped = datas.groupby(['key_size', 'threads']).LATENCY.mean().reset_index()
-for thread in grouped['threads'].unique():
-    subset = grouped[grouped['threads'] == thread]
-    plt.plot(subset['key_size'], subset['LATENCY'], label=f"Threads: {thread}", marker='o')
+    for thread in grouped['threads'].unique():
+        subset = grouped[grouped['threads'] == thread]
+        plt.plot(subset['key_size'], subset['REQ'], label=f"Threads: {thread}", marker='o')
 
-plt.legend()
-plt.xlabel("Key Size (bytes)")
-plt.ylabel("Latency (ms)")
-plt.title("Average Latency vs. Key Size for Different Number of Threads")
-plt.grid(True)
-plt.savefig("./plots/latency_vs_keysize_threads.PNG")
-plt.close()
+    plt.legend()
+    plt.xlabel("Key Size (bytes)")
+    plt.ylabel("Average Request (ms)")
+    plt.title(f"Average Request vs. Key Size for Different Number of Threads (Debits: {debits})")
+    plt.grid(True)
+    plt.savefig(f"./plots/request_vs_keysize_threads_debits_{debits}.PNG")
+    plt.close()
 
+for debits, group_data in grouped_debits:
+    plt.figure(figsize=(10, 6))
+    # Grouper par 'key_size' et 'threads' puis calculer la latence moyenne
+    grouped = group_data.groupby(['file_size', 'threads'])['LATENCY'].mean().reset_index()
 
-# Grouper par file_size et threads puis calcule la latence moyenne
-grouped = datas.groupby(['file_size', 'threads']).LATENCY.mean().reset_index()
-for thread in grouped['threads'].unique():
-    subset = grouped[grouped['threads'] == thread]
-    plt.plot(subset['file_size'], subset['LATENCY'], label=f"Threads: {thread}", marker='o')
+    for thread in grouped['threads'].unique():
+        subset = grouped[grouped['threads'] == thread]
+        plt.plot(subset['file_size'], subset['LATENCY'], label=f"Threads: {thread}", marker='o')
 
-plt.legend()
-plt.xlabel("File Size (bytes)")
-plt.ylabel("Latency (ms)")
-plt.title("Average Latency vs. File Size for Different Number of Threads")
-plt.grid(True)
-plt.savefig("./plots/latency_vs_filesize_threads.PNG")
-plt.close()
+    plt.legend()
+    plt.xlabel("File Size (bytes)")
+    plt.ylabel("Average Latency (ms)")
+    plt.title(f"Average Latency vs. File Size for Different Number of Threads (Debits: {debits})")
+    plt.grid(True)
+    plt.savefig(f"./plots/latency_vs_filesize_threads_debits_{debits}.PNG")
+    plt.close()
+
+for debits, group_data in grouped_debits:
+    plt.figure(figsize=(10, 6))
+    # Grouper par 'key_size' et 'threads' puis calculer la latence moyenne
+    grouped = group_data.groupby(['file_size', 'threads'])['REQ'].mean().reset_index()
+
+    for thread in grouped['threads'].unique():
+        subset = grouped[grouped['threads'] == thread]
+        plt.plot(subset['file_size'], subset['REQ'], label=f"Threads: {thread}", marker='o')
+
+    plt.legend()
+    plt.xlabel("File Size (bytes)")
+    plt.ylabel("Average Request (ms)")
+    plt.title(f"Average Request vs. File Size for Different Number of Threads (Debits: {debits})")
+    plt.grid(True)
+    plt.savefig(f"./plots/request_vs_filesize_threads_debits_{debits}.PNG")
+    plt.close()
