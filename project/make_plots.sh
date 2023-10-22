3d106d8d378f0753ce68a8b004b9a6537af759c5
Naya-01 <abarkan.rayan@gmail.com>
2023-10-22 22:56:30 +0200

    cleans project & uncomment lines in bash script

--- project/make_plots.sh
+++ project/make_plots.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#make -C "./server_implementation" run_inginious &
+make -C "./server_implementation" run_inginious &
 
 echo "Executing performance evaluation for generated Lua scripts..."
 
-# lua "./wrk_scripts/test.lua"
+lua "./wrk_scripts/test.lua"
 
 # echo "Performance evaluation completed."
 
-# # tests
-# npf-run --test test.npf --single-output "./data/results.csv"
+# tests
+npf-run --test test.npf --single-output "./data/results.csv"
 
 # Appeler le script Python pour générer les graphiques
 python "python_scripts/scriptCsv.py"
 
 
 
-# PID=$(lsof -t -i :8888)
-# if [ ! -z "$PID" ]; then
-#     kill -9 $PID
-#     echo "Processus avec PID $PID a été tué."
-# else
-#     echo "Aucun processus n'écoute sur le port 8888."
-# fi
+PID=$(lsof -t -i :8888)
+if [ ! -z "$PID" ]; then
+    kill -9 $PID
+    echo "Processus avec PID $PID a été tué."
+else
+    echo "Aucun processus n'écoute sur le port 8888."
+fi
