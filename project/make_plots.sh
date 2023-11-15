5cbb8484ff223e0d08ddb8376c0597fa960fd56f
Naya-01 <abarkan.rayan@gmail.com>
2023-11-15 17:44:56 +0100

    update

--- project/make_plots.sh
+++ project/make_plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make -C "./server_implementation" run_inginious &
+#make -C "./server_implementation" run_inginious &
 
 lua "./wrk_scripts/test.lua"
 
@@ -10,10 +10,10 @@ npf-run --test test.npf --single-output "./data/results.csv"
 # # Appeler le script Python pour générer les graphiques
 python3 "python_scripts/plots_p3.py"
 
-PID=$(lsof -t -i :8888)
-if [ ! -z "$PID" ]; then
-    kill -9 $PID
-    echo "Processus avec PID $PID a été tué."
-else
-    echo "Aucun processus n'écoute sur le port 8888."
-fi
+# PID=$(lsof -t -i :8888)
+# if [ ! -z "$PID" ]; then
+#     kill -9 $PID
+#     echo "Processus avec PID $PID a été tué."
+# else
+#     echo "Aucun processus n'écoute sur le port 8888."
+# fi
